498632a68db4ac0798c100110f1ed2059f77c46d
Solly Varcoe <sollyvarcoe@gmail.com>
2018-06-14 18:00:16 +0100

    added oscilliscope

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
-var canvas = document.querySelector('canvas')
+var canvas = document.getElementById('canvas1');
+var canvasSecond = document.getElementById('canvas2');
 //Sets canvas to size of window
-canvas.width = 1024;
-canvas.height = 1024;
-canvas
+canvas.width = 768;
+canvas.height = 768;
+canvasSecond.width = 768;
+canvasSecond.height = 768;
+
 var c = canvas.getContext("2d");
+var c2 = canvasSecond.getContext("2d");
 var ctx = new AudioContext();
 var audio = document.getElementById('myAudio');
 var audioSrc = ctx.createMediaElementSource(audio);
@@ -16,17 +20,35 @@ audioSrc.connect(ctx.destination);
 
 // frequencyBinCount tells you how many values you'll receive from the analyser
 var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+var timeData = new Uint8Array(analyser.frequencyBinCount);
 var size = frequencyData.length
 var lenght = window.innerWidth/size;
 
-function draw() {
-  for (i = 0; i < size; i++) {
-    let ampilitude = frequencyData[i]*2;
+function drawFreq() {
+  for (i = 0; i < size/8; i++) {
+    let ampilitude = frequencyData[i*8]*2;
     c.beginPath();
     c.strokeStyle="white";
-    c.moveTo(i,canvas.height);
-    c.lineTo(i,canvas.height-ampilitude);
+    c.lineWidth = 4;
+    c.moveTo(i*8 + 8,canvas.height);
+    c.lineTo(i*8 + 8,canvas.height-ampilitude);
     c.stroke();
+
+  }
+}
+function drawTime() {
+  for (i = 0; i < size/2; i++) {
+    let ampilitude = timeData[i*2];
+    c2.beginPath();
+    if (audio.paused) c2.strokeStyle="black";
+    else c2.strokeStyle="white";
+    c2.lineWidth = 4;
+    c2.moveTo(i*2 + 8,canvasSecond.height/2);
+    c2.lineTo(i*2 + 8,canvasSecond.height/2-ampilitude/2);
+    c2.stroke();
+    c2.moveTo(i*2 + 8,canvasSecond.height/2);
+    c2.lineTo(i*2 + 8,canvasSecond.height/2+ampilitude/2);
+    c2.stroke();
 
   }
 }
@@ -35,17 +57,22 @@ function renderFrame() {
    requestAnimationFrame(renderFrame);
    // update data in frequencyData
    c.clearRect(0,0,canvas.width,canvas.height);
+   c2.clearRect(0,0,canvas.width,canvas.height);
    analyser.getByteFrequencyData(frequencyData);
+   analyser.getByteTimeDomainData(timeData);
    console.log(frequencyData);
-   draw();
+   drawFreq();
+   drawTime();
    // render frame based on values in frequencyData
-   // console.log(frequencyData)
+   // console.log(frequencyData
 }
 
 window.addEventListener("click", function() {
   if (audio.paused) audio.play();
   else audio.pause();
+
 } )
+
 
 console.log(frequencyData);
 console.log(size);
